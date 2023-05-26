e4e67ad7c07a7415bfbb57edf51079c566c1cad3
bruuhcx0731 <133427165+bruuhcx0731@users.noreply.github.com>
2023-05-26 13:53:53 -0400

    Add files via upload

---
+++ events.js
@@ -0,0 +1,32 @@
+
+document.getElement
+
+const app = Vue.createApp({
+  data() {
+    return {
+      counter: 0,
+      name: '',
+      confirmedName: '',
+    };
+  },
+  methods: {
+    add(num) {
+      this.counter = this.counter + num;
+    },
+    subtract(num) {
+      this.counter = this.counter - num;
+    },
+    setName(event, lastName) {
+      this.name = event.target.value + ' ' + lastName;
+    },
+    confirmInput(){
+      this.confirmedName = this.name;
+    },
+    submitForm(event) {
+      alert('Submitted!');    
+    }
+
+  }
+});
+
+app.mount('#events');
